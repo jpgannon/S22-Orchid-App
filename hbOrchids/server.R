56616b912ac409e7918cb8cbc3e64d1eba826866
Kira Lee <kira4@vt.edu>
2022-02-11 21:14:58 -0500

    basic leaflet map

--- hbOrchids/server.R
+++ hbOrchids/server.R
@@ -1,28 +1,26 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+#install.packages("googlesheets4")
+#install.packages("leaflet.providers")
+#install.packages("leaflet.providers")
 
+#shiny leaflet tutorial
+#https://github.com/SimonGoring/ShinyLeaflet-tutorial/blob/master/Shiny-leaflet-tutorial.Rmd
 
-#HERE IS MY EDIT 
-#i hope this works
 library(shiny)
+library(leaflet)
+library("googlesheets4")
 
-# Define server logic required to draw a histogram
+
+#read parking lot google sheet
+#parking <- read_sheet("https://docs.google.com/spreadsheets/d/1tMqjQqi3NKxpOhHTp9JcWYGMEhGMWmAUsw8L6n_hiUE/edit#gid=1185719056")
+
+# Define server logic required to draw a map, calculate best paths
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
+  output$mapPlot <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.Terrain) %>% #sets basemap
+      setView(lng = -71.746866, lat = 43.942395, zoom = 15)  #sets location
+  })
+  
 
 })
--- hbOrchids/ui.R
+++ hbOrchids/ui.R
@@ -1,35 +1,19 @@
-#testtest
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#install.packages(leaflet)
 
 library(shiny)
+library(leaflet)
 
-# Define UI for application that draws a histogram
+
+
+
+# Define UI for application maps orchid paths
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+    titlePanel("Orchid Path Finder"),
+  
+    leafletOutput("mapPlot")
+   
     
-    #test edit
+    
 ))

a9af2550300469f03b441ae4459a97dd0cafb776
Kira Lee <kira4@vt.edu>
2022-02-23 02:03:47 -0500

    split into pages, added orchid data, added buttons and input selections

--- hbOrchids/server.R
+++ hbOrchids/server.R
@@ -11,9 +11,9 @@ library("DT")
 #MAKE SURE THE GOOGLE SHEETS PERMISSIONS ARE CHANGED TO "READABLE BY ANYONE WITH LINK"
 gs4_deauth()
 
-#read parking lot google sheet
-parking 
+#reads data from google sheets
 parking <- read_sheet("https://docs.google.com/spreadsheets/d/1tMqjQqi3NKxpOhHTp9JcWYGMEhGMWmAUsw8L6n_hiUE/edit#gid=1185719056")
+orchid <- read_sheet("https://docs.google.com/spreadsheets/d/1Celap5Y1edXb2xly_9HDc9R7hdPIjZ8qPNwxh59PryM/edit?usp=sharing")
 
 # Define server logic required to draw a map, calculate best paths
 shinyServer(function(input, output) {
@@ -27,13 +27,13 @@ shinyServer(function(input, output) {
     
     #plots points
     leafletProxy("mapPlot") %>% clearMarkers() %>%
-        addMarkers(lng = parking$lon,
-                         lat = parking$lat,
-                         popup = parking$parking)
+        addMarkers(lng = orchid$lon,
+                         lat = orchid$lat,
+                         popup = orchid$orchid_id)
     
     
     # outputs a table
-    output$x12 = DT::renderDataTable(parking, server = FALSE)
+    output$orch = DT::renderDataTable(orchid[1:5], filter = "top", server = FALSE)
     
     # output$x4 = renderPrint({
     #   s = input$x12_rows_selected
--- hbOrchids/ui.R
+++ hbOrchids/ui.R
@@ -1,34 +1,64 @@
 #install.packages(leaflet)
 #install.packages("googlesheets4")
-
+#install.packages("shinythemes")
 
 library(shiny)
 library(leaflet)
 library("googlesheets4")
+library("shinythemes")
+
 
 
 
 # Define UI for application maps orchid paths
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Orchid Path Finder"),
-
-    fluidRow(
-      column(9, verbatimTextOutput('x4')),
-      
-      
-      #outputs a table, can make multiple selections
-      column(
-        3, h3('Parking Locations (placeholder)'), hr(),
-        DT::dataTableOutput('x12')
-      )
-      
-    ),
-    
-    hr(), #adds horizontal spacing
-    
-    #outputs the map1
-    
-    leafletOutput("mapPlot")
+  
+  navbarPage("Orchid Path Finder", theme = shinytheme("flatly"),
+             #Routes Page
+             tabPanel("Routes",
+                      #section 1
+                      fluidRow(
+                        column(6, 
+                               #dropdown filters
+                               wellPanel(
+                                 h3('Filters'),
+                                 selectInput('visitGroups', 'Select Visit Group', choices = c("",orchid$visit_grp)),
+                                 selectInput('site', 'Select Site', choices = c("",orchid$site))
+                               )
+                        ),
+                        column(3, 
+                               br(), #vertical spacing
+                               br(),
+                               br(),
+                               actionButton("selectAll", "Select All"),
+                               actionButton("addList", "Add to List")),
+                        column(3,
+                               br(),
+                               br(),
+                               br(),
+                               actionButton("resultsPage", "Generate"))
+                      ),
+                      
+                      #section 2
+                      fluidRow(
+                        #outputs a table, can make multiple selections
+                        column(
+                          6, h3('Orchids'), 
+                          hr(),
+                          DT::dataTableOutput('orch')
+                        ),
+                        column(6, h3('Selected Orchids'),
+                               hr(),
+                               #filtered selections
+                        )
+                      )
+                      
+                      
+             ),
+             #Results page
+             tabPanel("Results",
+                      #outputs the map
+                      leafletOutput("mapPlot"))
+  )
+  
 ))
